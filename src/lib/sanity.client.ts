9083c9c4d67ec29af956ffce9dd444e0faeb586b
Wojciech Turek <contact@wojciechturek.com>
2023-02-05 17:47:16 +0100

    Add script to run sanity from root adn cdn setting for prod only

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -9,7 +9,7 @@ export const sanityClient = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn: process.env.NODE_ENV === "production",
 });
 
 export const urlFor = (source: any) =>
