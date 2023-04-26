316f447dd416ee92020c85a52eda6cfbb9ba605f
wojciech-turek <contact@wojciechturek.com>
2023-04-26 15:02:00 +0200

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <Analytics />
         <Component {...pageProps} />
       </QueryClientProvider>
     </>
